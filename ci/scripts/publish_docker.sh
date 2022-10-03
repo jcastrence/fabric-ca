aedaa4658579dc1a6c96667c347e0299a24ac6ed
Julian Castrence <juliancastrence@ibm.com>
2022-10-03 14:27:54 -0400

    Release Workflow
    Signed-off-by: Julian Castrence <juliancastrence@ibm.com>

--- ci/scripts/publish_docker.sh
+++ ci/scripts/publish_docker.sh
@@ -8,14 +8,14 @@ make docker
 
 docker login --username "${DOCKER_USERNAME}" --password "${DOCKER_PASSWORD}"
 
-docker tag "hyperledger/fabric-ca" "hyperledger/fabric-ca:amd64-${RELEASE}"
-docker push "hyperledger/fabric-ca:amd64-${RELEASE}"
+docker tag "hyperledger/fabric-ca" "jcibm12345/fabric-ca:amd64-${RELEASE}"
+docker push "jcibm12345/fabric-ca:amd64-${RELEASE}"
 
-docker tag "hyperledger/fabric-ca" "hyperledger/fabric-ca:${RELEASE}"
-docker push "hyperledger/fabric-ca:${RELEASE}"
+docker tag "hyperledger/fabric-ca" "jcibm12345/fabric-ca:${RELEASE}"
+docker push "jcibm12345/fabric-ca:${RELEASE}"
 
-docker tag "hyperledger/fabric-ca" "hyperledger/fabric-ca:amd64-${TWO_DIGIT_RELEASE}"
-docker push "hyperledger/fabric-ca:amd64-${TWO_DIGIT_RELEASE}"
+docker tag "hyperledger/fabric-ca" "jcibm12345/fabric-ca:amd64-${TWO_DIGIT_RELEASE}"
+docker push "jcibm12345/fabric-ca:amd64-${TWO_DIGIT_RELEASE}"
 
-docker tag "hyperledger/fabric-ca" "hyperledger/fabric-ca:${TWO_DIGIT_RELEASE}"
-docker push "hyperledger/fabric-ca:${TWO_DIGIT_RELEASE}"
+docker tag "hyperledger/fabric-ca" "jcibm12345/fabric-ca:${TWO_DIGIT_RELEASE}"
+docker push "jcibm12345/fabric-ca:${TWO_DIGIT_RELEASE}"
